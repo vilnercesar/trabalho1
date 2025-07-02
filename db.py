042fa7afd1203a585797fa2c6a927270e93a0a2b
Vilner César <vilnercesar@gmail.com>
2025-07-02 11:03:43 -0300

    atualizando configurações

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 
-DATABASE_URL = "postgresql+psycopg2://postgres:admin@localhost:5432/lib_bd" # Substitua pelos seus 
+DATABASE_URL = "postgresql+psycopg2://postgres:senha@localhost:5432/lib_bd" # Substitua pelos seus 
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
